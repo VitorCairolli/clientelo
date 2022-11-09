9ae899323266dceb870d94e6a6325f20722fd5f8
VitorCairolli <cairollivitor@gmail.com>
2022-11-09 17:24:48 -0300

    Adicionado classe 'CategoryDao'

--- src/main/java/br/com/alura/clientelo/Main.java
+++ src/main/java/br/com/alura/clientelo/Main.java
@@ -3,6 +3,7 @@ package br.com.alura.clientelo;
 import br.com.alura.clientelo.controllers.ReportController;
 import br.com.alura.clientelo.converters.CsvOrderConverter;
 import br.com.alura.clientelo.converters.JsonOrderConverter;
+import br.com.alura.clientelo.daos.CategoryDao;
 import br.com.alura.clientelo.daos.ClientDao;
 import br.com.alura.clientelo.daos.ProductDao;
 import br.com.alura.clientelo.models.*;
@@ -29,28 +30,6 @@ public class Main {
         reportController.logReport(new TopThreeOrdersByQuantityReport());
         reportController.logReport(new OrderCategoryReport());
         reportController.logReport(new MostValuableByOrderCategoryReport());
-
-        ProductDao productDao = new ProductDao();
-        Product product = new Product("Ukulele",
-                BigDecimal.TEN,
-                "Mt fofo",
-                23,
-                new Category("INSTRUMENTO"));
-
-        productDao.create(product);
-        product.setCategory(new Category("PAO"));
-        productDao.update(product);
-
-        ClientDao clientDao = new ClientDao();
-        Client client = (new Client("Sara",
-                "saravana@gmail.com",
-                new Address("Rua Sararara",
-                        "13",
-                        "sem comentarios")));
-
-        clientDao.create(client);
-        client.setName("Sara2");
-        clientDao.update(client);
     }
 }
 
